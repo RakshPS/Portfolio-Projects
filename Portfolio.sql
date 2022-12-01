405c5388daa9c726bcf451ba7c04d0825b557473
RakshPS <92082256+RakshPS@users.noreply.github.com>
2022-12-01 13:07:45 +0530

    Update Portfolio.sql

--- Portfolio.sql
+++ Portfolio.sql
@@ -1,4 +1,3 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
 --SELECT * from dbo.CovidDeaths$ ORDER BY 3,4;
 --SElect * from dbo.CovidVaccinations$ ORDEr by 3,4;
 --select data that is needed
@@ -21,32 +20,36 @@ select location, date,population, total_cases , Round((total_cases/population *1
 from Portfolio_projects.dbo.CovidDeaths$
 --where location LIKE '%india%'
 order by 5 DESC
+
 --countries with highest infection rate compared to its population
 select location, population, max(total_cases) as highest_infection_count, Round(MAX(total_cases/population *100),2) as highest_percent_of_infection
 from Portfolio_projects.dbo.CovidDeaths$
 where continent IS NOT NULL
 group by location, population
 order by highest_infection_count DESC
+
 --countries with highest death rate compared to its population
 select location, population, max(total_deaths) as highest_death_count, Round(MAX(total_deaths/population *100),2) as  highest_percent_of_death
 from Portfolio_projects.dbo.CovidDeaths$
 --where location like '%india%'
 group by location, population
 order by  highest_percent_of_death DESC
+
 --countries with highest death count per population
 select location, max(CAST(total_deaths AS INT)) as Total_deaths
 from Portfolio_projects.dbo.CovidDeaths$
 where continent IS NOT NULL
 group by location
 order by Total_deaths DESC
+
 --highest death count by continent
 select continent, max(Cast(total_deaths AS INT)) as Total_deaths
 from Portfolio_projects.dbo.CovidDeaths$
 where continent is not null
 group by continent 
 ORDER BY Total_deaths DESC
+
 --total cases & death percentage globally 
----using new cases & deaths as total cases is adding new cases to existing cases
 select sum(new_cases) as Total_cases, sum(CAST(new_deaths as int)) as 'Total Deaths',
 sum(CAST(new_deaths as int))/sum(new_cases) *100 as 'Death percentage'
 from Portfolio_projects..CovidDeaths$
@@ -77,7 +80,7 @@ and dea.date = vac.date
 select *, PeopleVaccinated/Population *100 as Vaccinated_Percentage
 from #Population_of_PeopleVaccination
 
---saving it as view for visualizations
+--saving it as view
 Create View Population_of_PeopleVaccination as
 select dea.continent, dea.location, dea.date,dea.population , vac.new_vaccinations,
 sum(CAST(vac.new_vaccinations as bigint)) OVER (Partition by dea.location ORDER BY dea.location, dea.date)
@@ -89,12 +92,46 @@ and dea.date = vac.date
 where dea.continent is not null
 
 select * from Population_of_PeopleVaccination
+
 --view2
 Create View Global_stats as
 select sum(new_cases) as Total_cases, sum(CAST(new_deaths as int)) as 'Total Deaths',
 sum(CAST(new_deaths as int))/sum(new_cases) *100 as 'Death percentage'
 from Portfolio_projects..CovidDeaths$
 select * from Global_stats
+
+--Tables for visualization using Tableau
+--Table 1:
+select sum(new_cases) as Total_cases, sum(CAST(new_deaths as int)) as 'Total Deaths',
+sum(CAST(new_deaths as int))/sum(new_cases) *100 as 'Death percentage'
+from Portfolio_projects..CovidDeaths$
+where continent is not null
+
+--checking the numbers using other way 
+--select sum(new_cases) as Total_cases, sum(CAST(new_deaths as int)) as 'Total Deaths',
+--sum(CAST(new_deaths as int))/sum(new_cases) *100 as 'Death percentage'
+--from Portfolio_projects..CovidDeaths$
+--where location = 'World'
+
+--Table 2:
+--continent wise total deathcount
+select continent, sum(Convert(int,new_deaths)) as 'Total Death Count'
+from Portfolio_projects..CovidDeaths$
+where continent is not null
+group by continent
+Order by [Total Death Count] DESC
+
+--Table 3:
+select location, population, max(total_cases) as HighestInfectionCount, (max(total_cases) / population) *100 as HighestInfectedPercentage
+from Portfolio_projects..CovidDeaths$
+group by location, population
+order by HighestInfectedPercentage DESC
+
+--Table 4:
+select location, population, date, max(total_cases) as HighestInfectionCount, (max(total_cases) / population) *100 as HighestInfectedPercentage
+from Portfolio_projects..CovidDeaths$
+group by location, population, date
+order by HighestInfectedPercentage DESC
 
 
 
